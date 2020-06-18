1b2d5875e461443a11808fafeb15da1fdde9ab40
Riccardo Finotello <riccardo.finotello@gmail.com>
2020-06-18 18:37:10 +0200

    Reproducible Data - Peer assesment 1 - Full analsysis
    Signed-off-by: Riccardo Finotello <riccardo.finotello@gmail.com>

--- PA1_template.md
+++ PA1_template.md
@@ -1,9 +1,9 @@
 ---
 title: 'Reproducible Research: Peer Assessment 1'
 output:
-  pdf_document: default
   html_document:
     keep_md: yes
+  pdf_document: default
 ---
 
 
@@ -12,7 +12,8 @@ output:
 Before adventuring further in the analysis, we unzip the file containing the
 data in order to access it.
 
-```{r unzip, echo = TRUE}
+
+```r
 file.zip <- file.path(".", "activity.zip")
 file.csv <- file.path(".", "activity.csv")
 if(file.exists(file.zip) & !file.exists(file.csv)) {unzip(file.zip)}
@@ -20,20 +21,31 @@ if(file.exists(file.zip) & !file.exists(file.csv)) {unzip(file.zip)}
 
 We then read the data using the `read.csv` function:
 
-```{r read, echo = TRUE}
+
+```r
 data <- read.csv(file.csv, header = TRUE, stringsAsFactors = FALSE)
 ```
 
 The table has dimensions
 
-```{r dim, echo = TRUE}
+
+```r
 dim(data)
+```
+
+```
+## [1] 17568     3
 ```
 
 and holds the variables
 
-```{r var, echo = TRUE}
+
+```r
 names(data)
+```
+
+```
+## [1] "steps"    "date"     "interval"
 ```
 
 representing the no. of **steps** made in a given **date** per time **interval**
@@ -42,15 +54,24 @@ representing the no. of **steps** made in a given **date** per time **interval**
 We transform the **date** column to become a _Date_ class to better handle its
 format later in the analysis:
 
-```{r transf, echo = TRUE}
+
+```r
 data$date <- as.Date(data$date, "%Y-%m-%d")
 ```
 
 Finally we convert the `data.frame` table in a `data.table` format for the sake
 of simplicity:
 
-```{r table, echo = TRUE}
+
+```r
 library(data.table)
+```
+
+```
+## Warning: package 'data.table' was built under R version 3.6.3
+```
+
+```r
 data <- data.table(data)
 ```
 
@@ -59,7 +80,8 @@ data <- data.table(data)
 The total number of steps can be computed using the `by` function in the
 `DT[i, j, by]` call:
 
-```{r tot.steps, echo = TRUE}
+
+```r
 data.steps <- data[,
                    .(steps.sum = sum(steps, na.rm = TRUE), wday = weekdays(date)),
                    by = date
@@ -68,17 +90,37 @@ data.steps <- data[,
 
 We can then compute the mean and median of the distributions:
 
-```{r median.mean, echo = TRUE}
+
+```r
 data.steps.median <- median(data.steps$steps.sum)
 data.steps.mean   <- mean(data.steps$steps.sum)
 print(paste("Median:", data.steps.median))
+```
+
+```
+## [1] "Median: 10395"
+```
+
+```r
 print(paste("Mean:", data.steps.mean))
+```
+
+```
+## [1] "Mean: 9354.22950819672"
 ```
 
 Finally we can plot the total no. of steps **taken in each day**:
 
-```{r tot.plot, echo = TRUE}
+
+```r
 library(ggplot2)
+```
+
+```
+## Warning: package 'ggplot2' was built under R version 3.6.3
+```
+
+```r
 plot.steps <- ggplot(data = data.steps, aes(x = date, y = steps.sum, fill = wday)) +
               scale_x_date(breaks = data.steps$date[seq(1, length(data.steps$date), 7)]) +
               theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
@@ -94,19 +136,23 @@ plot.steps <- ggplot(data = data.steps, aes(x = date, y = steps.sum, fill = wday
 print(plot.steps)
 ```
 
+![](PA1_template_files/figure-html/tot.plot-1.png)<!-- -->
+
 ## What is the average daily activity pattern?
 
 The daily activity pattern is connected to the average no. of steps taken in
 each 5-minute interval. We therefore group by that variable to compute the
 average no. of steps:
 
-```{r avg.steps, echo = TRUE}
+
+```r
 data.avg <- data[, .(steps.avg = mean(steps, na.rm = TRUE)), by = interval]
 ```
 
 Given the data, we can therefore plot a time series of such variable:
 
-```{r time.series, echo = TRUE}
+
+```r
 plot.avg <- ggplot(data = data.avg, aes(x = interval, y = steps.avg)) +
             geom_step(direction = "hv") +
             xlab("interval") +
@@ -116,12 +162,19 @@ plot.avg <- ggplot(data = data.avg, aes(x = interval, y = steps.avg)) +
 print(plot.avg)
 ```
 
+![](PA1_template_files/figure-html/time.series-1.png)<!-- -->
+
 Finally we compute the maximum steps taken in average grouped by intervals:
 
-```{r max.avg, echo = TRUE}
+
+```r
 max.pos <- which.max(data.avg$steps.avg)
 max.val <- data.avg[max.pos,]$interval
 print(paste("Max no. of steps (", max.val, ") in the ", max.pos, "th time step", sep = ""))
+```
+
+```
+## [1] "Max no. of steps (835) in the 104th time step"
 ```
 
 ## Imputing missing values
@@ -129,7 +182,8 @@ print(paste("Max no. of steps (", max.val, ") in the ", max.pos, "th time step",
 Missing values can be found using the `is.na` function inside the `data.table`
 call:
 
-```{r is.na, echo = TRUE}
+
+```r
 na.steps <- sum(as.numeric(is.na(data$steps)))
 na.dates <- sum(as.numeric(is.na(data$date)))
 na.intvs <- sum(as.numeric(is.na(data$interval)))
@@ -140,7 +194,18 @@ print(paste("Total no. of NA values in: ",
             sep = ""
            )
      )
+```
+
+```
+## [1] "Total no. of NA values in: step --> 2304, date --> 0, interval --> 0."
+```
+
+```r
 print(paste("Total no. of rows with NA values:", dim(data[is.na(data$steps)])[1]))
+```
+
+```
+## [1] "Total no. of rows with NA values: 2304"
 ```
 
 We then create a new dataset using the information on the missing values: we
@@ -148,7 +213,8 @@ first merge by interval to assign each **step** variable its corresponding
 average value, then we substitute **only** the _NA_ values with such
 **average**.
 
-```{r new, echo = TRUE}
+
+```r
 data.byinterval <- merge(data, data.avg, by = "interval")
 data.byinterval <- data.byinterval[order(date, interval),]
 data.byinterval$steps <- as.double(data.byinterval$steps)
@@ -157,18 +223,35 @@ data.byinterval <- data.byinterval[is.na(steps), steps := steps.avg]
 
 The new dataset looks like
 
-```{r new.head, echo = TRUE}
+
+```r
 head(data.byinterval)
 ```
 
+```
+##    interval     steps       date steps.avg
+## 1:        0 1.7169811 2012-10-01 1.7169811
+## 2:        5 0.3396226 2012-10-01 0.3396226
+## 3:       10 0.1320755 2012-10-01 0.1320755
+## 4:       15 0.1509434 2012-10-01 0.1509434
+## 5:       20 0.0754717 2012-10-01 0.0754717
+## 6:       25 2.0943396 2012-10-01 2.0943396
+```
+
 of dimension
-```{r new.dim, echo = TRUE}
+
+```r
 dim(data.byinterval)
+```
+
+```
+## [1] 17568     4
 ```
 
 We then recompute the total no. of steps taken each day with filled values
 
-```{r new.steps, echo = TRUE}
+
+```r
 data.steps.fill <- data.byinterval[,
                                    .(steps.sum = sum(steps, na.rm = TRUE),
                                    wday = weekdays(date)
@@ -179,11 +262,23 @@ data.steps.fill <- data.byinterval[,
 
 Mean and median have then to be recomputed:
 
-```{r new.mean.median, echo = TRUE}
+
+```r
 data.steps.fill.median <- median(data.steps.fill$steps.sum)
 data.steps.fill.mean   <- mean(data.steps.fill$steps.sum)
 print(paste("Median:", data.steps.fill.median))
+```
+
+```
+## [1] "Median: 10766.1886792453"
+```
+
+```r
 print(paste("Mean:", data.steps.fill.mean))
+```
+
+```
+## [1] "Mean: 10766.1886792453"
 ```
 
 thus showing than **median and mean are now extremely similar**, since we input a
@@ -191,7 +286,8 @@ lot of entries taking exactly the mean as value.
 
 Finally the new plot shows possible differences from the previous case:
 
-```{r new.plot, echo = TRUE}
+
+```r
 plot.steps.fill <- ggplot(data = data.steps.fill, aes(x = date, y = steps.sum, fill = wday)) +
                           scale_x_date(breaks = data.steps.fill$date[seq(1, length(data.steps.fill$date), 7)]) +
                           theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
@@ -207,6 +303,8 @@ plot.steps.fill <- ggplot(data = data.steps.fill, aes(x = date, y = steps.sum, f
 print(plot.steps.fill)
 ```
 
+![](PA1_template_files/figure-html/new.plot-1.png)<!-- -->
+
 
 ## Are there differences in activity patterns between weekdays and weekends?
 
@@ -214,7 +312,8 @@ In order to study differences coming from weekdays and weekends, we first add
 a **factor** variable to distinguish Mondays through Fridays and Saturdays to
 Sundays.
 
-```{r days, echo = TRUE}
+
+```r
 day.factor <- format(data.byinterval$date, "%u")
 day.factor[day.factor %in% 1:5] <- "Weekday"
 day.factor[day.factor %in% 6:7] <- "Weekend"
@@ -225,7 +324,8 @@ data.byinterval$day <- day.factor
 We then group the data by interval and type of day to compute possible
 differences:
 
-```{r days.group, echo = TRUE}
+
+```r
 data.byday <- data.byinterval[,
                               .(steps.avg = mean(steps, na.rm = TRUE)),
                               by = list(interval, day)
@@ -234,7 +334,8 @@ data.byday <- data.byinterval[,
 
 Finally we plot the data divided into _weekday_ and _weekend_ side-by-side:
 
-```{r days.plot, echo = TRUE}
+
+```r
 plot.avg.byday <- ggplot(data = data.byday, aes(x = interval, y = steps.avg)) +
                   geom_step(direction = "hv") +
                   facet_grid(. ~ day) + 
@@ -244,6 +345,8 @@ plot.avg.byday <- ggplot(data = data.byday, aes(x = interval, y = steps.avg)) +
 
 print(plot.avg.byday)
 ```
+
+![](PA1_template_files/figure-html/days.plot-1.png)<!-- -->
 
 It seems that during **weekends** the no. of steps drops in the maximum absolute
 peak but it gets more substantially consistent throughout the day.
--- run_analysis.R
+++ run_analysis.R
@@ -1,84 +1,37 @@
----
-title: 'Reproducible Research: Peer Assessment 1'
-output:
-  pdf_document: default
-  html_document:
-    keep_md: yes
----
-
-
-## Loading and preprocessing the data
-
-Before adventuring further in the analysis, we unzip the file containing the
-data in order to access it.
-
-```{r unzip, echo = TRUE}
+# Unzip the file with the data
 file.zip <- file.path(".", "activity.zip")
 file.csv <- file.path(".", "activity.csv")
 if(file.exists(file.zip) & !file.exists(file.csv)) {unzip(file.zip)}
-```
 
-We then read the data using the `read.csv` function:
-
-```{r read, echo = TRUE}
+# Read the CSV file
 data <- read.csv(file.csv, header = TRUE, stringsAsFactors = FALSE)
-```
 
-The table has dimensions
+# Show first few lines
+head(data)
 
-```{r dim, echo = TRUE}
-dim(data)
-```
-
-and holds the variables
-
-```{r var, echo = TRUE}
-names(data)
-```
-
-representing the no. of **steps** made in a given **date** per time **interval**
-(5 minutes span).
-
-We transform the **date** column to become a _Date_ class to better handle its
-format later in the analysis:
-
-```{r transf, echo = TRUE}
+# Transform data in columns (transform dates, steps and intervals are fine)
 data$date <- as.Date(data$date, "%Y-%m-%d")
-```
 
-Finally we convert the `data.frame` table in a `data.table` format for the sake
-of simplicity:
-
-```{r table, echo = TRUE}
+# Convert table to data.table for improved manipulation
 library(data.table)
 data <- data.table(data)
-```
 
-## What is the mean total number of steps taken per day?
-
-The total number of steps can be computed using the `by` function in the
-`DT[i, j, by]` call:
-
-```{r tot.steps, echo = TRUE}
+# Compute the total no. of steps per date and plot a histogram
+library(ggplot2)
 data.steps <- data[,
-                   .(steps.sum = sum(steps, na.rm = TRUE), wday = weekdays(date)),
+                   .(steps.sum = sum(steps, na.rm = TRUE),
+                     wday = weekdays(date)
+                   ),
                    by = date
                   ]
-```
 
-We can then compute the mean and median of the distributions:
-
-```{r median.mean, echo = TRUE}
+# Compute mean and median of the total no. of steps
 data.steps.median <- median(data.steps$steps.sum)
 data.steps.mean   <- mean(data.steps$steps.sum)
 print(paste("Median:", data.steps.median))
 print(paste("Mean:", data.steps.mean))
-```
 
-Finally we can plot the total no. of steps **taken in each day**:
-
-```{r tot.plot, echo = TRUE}
-library(ggplot2)
+# Plot the resulting bar plots
 plot.steps <- ggplot(data = data.steps, aes(x = date, y = steps.sum, fill = wday)) +
               scale_x_date(breaks = data.steps$date[seq(1, length(data.steps$date), 7)]) +
               theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
@@ -91,45 +44,26 @@ plot.steps <- ggplot(data = data.steps, aes(x = date, y = steps.sum, fill = wday
               geom_hline(yintercept = data.steps.mean, linetype = "dashed") +
               scale_linetype_manual(name = "statistics") +
               ggtitle("Total no. of steps in each day")
-print(plot.steps)
-```
+#print(plot.steps)
 
-## What is the average daily activity pattern?
-
-The daily activity pattern is connected to the average no. of steps taken in
-each 5-minute interval. We therefore group by that variable to compute the
-average no. of steps:
-
-```{r avg.steps, echo = TRUE}
+# Compute average no. of steps
 data.avg <- data[, .(steps.avg = mean(steps, na.rm = TRUE)), by = interval]
-```
 
-Given the data, we can therefore plot a time series of such variable:
-
-```{r time.series, echo = TRUE}
+# Plot the time series of the data
 plot.avg <- ggplot(data = data.avg, aes(x = interval, y = steps.avg)) +
             geom_step(direction = "hv") +
             xlab("interval") +
             ylab("average no. of steps") +
             ggtitle("Average no. of steps per interval")
 
-print(plot.avg)
-```
+#print(plot.avg)
 
-Finally we compute the maximum steps taken in average grouped by intervals:
-
-```{r max.avg, echo = TRUE}
+# Find the max no. of steps
 max.pos <- which.max(data.avg$steps.avg)
 max.val <- data.avg[max.pos,]$interval
 print(paste("Max no. of steps (", max.val, ") in the ", max.pos, "th time step", sep = ""))
-```
 
-## Imputing missing values
-
-Missing values can be found using the `is.na` function inside the `data.table`
-call:
-
-```{r is.na, echo = TRUE}
+# Find total no. of missing data
 na.steps <- sum(as.numeric(is.na(data$steps)))
 na.dates <- sum(as.numeric(is.na(data$date)))
 na.intvs <- sum(as.numeric(is.na(data$interval)))
@@ -141,57 +75,24 @@ print(paste("Total no. of NA values in: ",
            )
      )
 print(paste("Total no. of rows with NA values:", dim(data[is.na(data$steps)])[1]))
-```
 
-We then create a new dataset using the information on the missing values: we
-first merge by interval to assign each **step** variable its corresponding
-average value, then we substitute **only** the _NA_ values with such
-**average**.
-
-```{r new, echo = TRUE}
+# Merge data and its average by interval (this way to each interval we associate an avg)
 data.byinterval <- merge(data, data.avg, by = "interval")
 data.byinterval <- data.byinterval[order(date, interval),]
 data.byinterval$steps <- as.double(data.byinterval$steps)
 data.byinterval <- data.byinterval[is.na(steps), steps := steps.avg]
-```
 
-The new dataset looks like
-
-```{r new.head, echo = TRUE}
+# Show the first few lines of the new dataset
 head(data.byinterval)
-```
 
-of dimension
-```{r new.dim, echo = TRUE}
-dim(data.byinterval)
-```
-
-We then recompute the total no. of steps taken each day with filled values
-
-```{r new.steps, echo = TRUE}
+# Recompute the total no. of steps
 data.steps.fill <- data.byinterval[,
                                    .(steps.sum = sum(steps, na.rm = TRUE),
                                    wday = weekdays(date)
                                    ),
                                   by = date
                                  ]
-```
-
-Mean and median have then to be recomputed:
-
-```{r new.mean.median, echo = TRUE}
-data.steps.fill.median <- median(data.steps.fill$steps.sum)
-data.steps.fill.mean   <- mean(data.steps.fill$steps.sum)
-print(paste("Median:", data.steps.fill.median))
-print(paste("Mean:", data.steps.fill.mean))
-```
-
-thus showing than **median and mean are now extremely similar**, since we input a
-lot of entries taking exactly the mean as value.
-
-Finally the new plot shows possible differences from the previous case:
-
-```{r new.plot, echo = TRUE}
+# Plot again the total no. of steps
 plot.steps.fill <- ggplot(data = data.steps.fill, aes(x = date, y = steps.sum, fill = wday)) +
                           scale_x_date(breaks = data.steps.fill$date[seq(1, length(data.steps.fill$date), 7)]) +
                           theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
@@ -204,37 +105,27 @@ plot.steps.fill <- ggplot(data = data.steps.fill, aes(x = date, y = steps.sum, f
                           geom_hline(yintercept = data.steps.fill.mean, linetype = "dashed") +
                           scale_linetype_manual(name = "statistics") +
                           ggtitle("Total no. of steps in each day (NA filled w/ interval mean)")
-print(plot.steps.fill)
-```
+#print(plot.steps.fill)
 
+# Recompute mean and median of the total no. of steps
+data.steps.fill.median <- median(data.steps.fill$steps.sum)
+data.steps.fill.mean   <- mean(data.steps.fill$steps.sum)
+print(paste("Median:", data.steps.fill.median))
+print(paste("Mean:", data.steps.fill.mean))
 
-## Are there differences in activity patterns between weekdays and weekends?
-
-In order to study differences coming from weekdays and weekends, we first add
-a **factor** variable to distinguish Mondays through Fridays and Saturdays to
-Sundays.
-
-```{r days, echo = TRUE}
+# Now add a variable to distinguish weekdays and weekends
 day.factor <- format(data.byinterval$date, "%u")
 day.factor[day.factor %in% 1:5] <- "Weekday"
 day.factor[day.factor %in% 6:7] <- "Weekend"
 day.factor <- factor(day.factor, levels = c("Weekday", "Weekend"))
 data.byinterval$day <- day.factor
-```
 
-We then group the data by interval and type of day to compute possible
-differences:
-
-```{r days.group, echo = TRUE}
+# Group by interval and compute the average step divided in weekday and weekend
 data.byday <- data.byinterval[,
                               .(steps.avg = mean(steps, na.rm = TRUE)),
                               by = list(interval, day)
                              ]
-```
 
-Finally we plot the data divided into _weekday_ and _weekend_ side-by-side:
-
-```{r days.plot, echo = TRUE}
 plot.avg.byday <- ggplot(data = data.byday, aes(x = interval, y = steps.avg)) +
                   geom_step(direction = "hv") +
                   facet_grid(. ~ day) + 
@@ -242,8 +133,4 @@ plot.avg.byday <- ggplot(data = data.byday, aes(x = interval, y = steps.avg)) +
                   ylab("average no. of steps") +
                   ggtitle("Average no. of steps per interval")
 
-print(plot.avg.byday)
-```
-
-It seems that during **weekends** the no. of steps drops in the maximum absolute
-peak but it gets more substantially consistent throughout the day.
+#print(plot.avg.byday)
